2edff749b1aa11a4c488fad4145c365052fc6d9a
inceserhatabdullah <inceserhatabdullah@gmail.com>
2026-06-08 14:47:32 +0300

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 sudo dnf groupinstall "Development Tools" -y
 sudo dnf install epel-release -y
 sudo dnf makecache
-sudo dnf install -y git zsh btop fzf bat ripgrep util-linux-user podman python3 python3-pip 
+sudo dnf install -y git zsh btop fzf bat ripgrep util-linux-user podman tree python3 python3-pip 
 pip install --user httpie
